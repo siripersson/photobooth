1821b336b32b17ddff6fc817dbd0239c4eb9c06e
siripersson <siri.persson1@gmail.com>
2017-09-28 15:17:45 +0200

    Add take snapshot function

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,8 @@
+
+const hidden_canvas = document.querySelector('canvas'); //Deklareras högst upp i dokumentet
 // Get video stream
 const video= document.getElementById('camera-stream')
+
 
 // Fetch video stream
 navigator.getMedia = (navigator.getUserMedia ||
@@ -17,22 +20,51 @@ navigator.getMedia( { video: true }, (stream) => {
 );
 
 
-// Eventlistener
-const take_photo_btn=document.querySelector('#take-photo')
+// Hämta ut knapparna
+const take_photo_btn=document.getElementById('take-photo')
+const save_photo_btn=document.getElementById('save-photo')
 
-take_photo_btn.addEventListener('click', (a) =>{
-	e.preventDefault();
+// Eventlistener
+take_photo_btn.addEventListener('click', (e) =>{
 	console.log("Clicked on take-photo")
+	const snap= takeSnapShot() //anropa snapchot funktionen, den returnerar en bild
+	const image_tag= document.getElementById('snap') // get image tag
+	image_tag.setAttribute("src", snap) // set attribute src
+	const save_photo_btn=document.getElementById('save-photo')
+	save_photo_btn.setAttribute("href",snap) // set attribute href
+	e.preventDefault();
 })
 
+// Eventlistener
+save_photo_btn.addEventListener('click', (e) =>{
+const save_photo_btn=document.getElementById('save-photo')
+	save_photo_btn.setAttribute("href",snap) // set attribute href
+	e.preventDefault();
+})
+
+
+//Function
 const takeSnapShot = () => {
 	// Read size, height and width
 	const width= document.getElementById('camera-stream').width;
 	const height= document.getElementById('camera-stream').height;
-	const size= document.getElementById('camera-stream').size;
+	//const size= document.getElementById('camera-stream').size;
 	console.log("Width: " + width)
 	console.log("Height: " + height)
-	console.log("Size: " + size)
+	//console.log("Size: " + size)
+
+	 // Grab elements, create settings, etc.
+    const context = hidden_canvas.getContext("2d")
+    context.drawImage(video,0,0,width,height)
+    //const picture= context.toDataURL('image/png')
+    const src=hidden_canvas.toDataURL("image/png") 
 }
 
-takeSnapShot()
+const saveSnapShot = () => {
+
+}
+
+
+//takeSnapShot()
+
+
